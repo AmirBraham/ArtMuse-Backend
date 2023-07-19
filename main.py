02ec34fdcd101aaf94b8182bd164389caf330d8c
amirbraham <amirbrahamm@gmail.com>
2023-07-19 20:43:44 +0100

    preprocessing metmuseum csv and scraping jpg links

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+PAINTINGS_COLLECTIONS = {
+    "metmuseum":"The Metropolitan Museum of Art"
+}
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/paintings_collections")
+async def paintings_collections():
+    return list(PAINTINGS_COLLECTIONS.values())
+
+@app.get("/{collection}/info")
+async def collection_info(collection):
+    if(collection in list(PAINTINGS_COLLECTIONS.keys())):
+        return {
+            "name":PAINTINGS_COLLECTIONS[f"{collection}"],
+            "paintings":100
+        }
+    return None
+
+@app.get("/{collection}/random")
+async def random(collection):
+    return None
+
--- preprocessing/metmuseum.py
+++ preprocessing/metmuseum.py
@@ -10,13 +10,14 @@ from tqdm import tqdm
 
 pd.set_option('display.max_colwidth', None)
 
-COLUMNS = ["Object Number","Is Highlight","Is Timeline Work","Is Public Domain","Object ID","Gallery Number","Department","AccessionYear","Object Name","Title","Culture","Period","Dynasty","Reign","Portfolio","Constituent ID","Artist Role","Artist Prefix","Artist Display Name","Artist Display Bio","Artist Suffix","Artist Alpha Sort","Artist Nationality","Artist Begin Date","Artist End Date","Artist Gender","Artist ULAN URL","Artist Wikidata URL","Object Date","Object Begin Date","Object End Date","Medium","Dimensions","Credit Line","Geography Type","City","State","County","Country","Region","Subregion","Locale","Locus","Excavation","River","Classification","Rights and Reproduction","Link Resource","Object Wikidata URL","Metadata Date","Repository","Tags","Tags AAT URL","Tags Wikidata URL"]
-
+COLUMNS = ["Object Number", "Is Highlight", "Is Timeline Work", "Is Public Domain", "Object ID", "Gallery Number", "Department", "AccessionYear", "Object Name", "Title", "Culture", "Period", "Dynasty", "Reign", "Portfolio", "Constituent ID", "Artist Role", "Artist Prefix", "Artist Display Name", "Artist Display Bio", "Artist Suffix", "Artist Alpha Sort", "Artist Nationality", "Artist Begin Date", "Artist End Date", "Artist Gender",
+           "Artist ULAN URL", "Artist Wikidata URL", "Object Date", "Object Begin Date", "Object End Date", "Medium", "Dimensions", "Credit Line", "Geography Type", "City", "State", "County", "Country", "Region", "Subregion", "Locale", "Locus", "Excavation", "River", "Classification", "Rights and Reproduction", "Link Resource", "Object Wikidata URL", "Metadata Date", "Repository", "Tags", "Tags AAT URL", "Tags Wikidata URL"]
 
 
 def calculateAspectRatio(row):
-    dimensions = re.findall(r'\((\d+(?:\.\d+)?) x (\d+(?:\.\d+)?) cm\)', str(row["Dimensions"]))
-    if(len(dimensions) ==0):
+    dimensions = re.findall(
+        r'\((\d+(?:\.\d+)?) x (\d+(?:\.\d+)?) cm\)', str(row["Dimensions"]))
+    if (len(dimensions) == 0):
         return 0
     dimensions = [(float(dim1), float(dim2)) for dim1, dim2 in dimensions]
     dimensions = dimensions[0]
@@ -24,31 +25,44 @@ def calculateAspectRatio(row):
     height = dimensions[0]
     return width/height
 
+
 def filter():
-    PHONE_ASPECT_RATIO = 9/16
-    data = pd.read_csv("../Database/metmuseum.csv",sep=",",low_memory=False)
-    paintings = data[data["Object Name"] == "Painting"] 
-    public_domain_paintings = paintings[paintings["Is Public Domain"]==True]
-    public_domain_paintings["ratio"] = public_domain_paintings.apply(calculateAspectRatio,axis=1)
-    public_domain_paintings = public_domain_paintings[abs(public_domain_paintings["ratio"] - PHONE_ASPECT_RATIO) < 0.1]
-    public_domain_paintings.to_csv("../Database/metmuseum_filtered.csv",encoding='utf-8', index=False)
+    ASPECT_RATIOS = [16/9, 16/10]
+    data = pd.read_csv("../Database/metmuseum.csv", sep=",", low_memory=False)
+    paintings = data[data["Object Name"] == "Painting"]
+    public_domain_paintings = paintings[paintings["Is Public Domain"] == True]
+    public_domain_paintings["ratio"] = public_domain_paintings.apply(
+        calculateAspectRatio, axis=1)
+    FILTERED_ASPECT_RATIO = [False] * len(public_domain_paintings)
+    THRESHOLD = 0.2
+    for a in ASPECT_RATIOS:
+        FILTERED_ASPECT_RATIO += abs(
+            public_domain_paintings["ratio"] - a) < THRESHOLD
+    FILTERED_ASPECT_RATIO = [i > 0 for i in FILTERED_ASPECT_RATIO]
+    public_domain_paintings = public_domain_paintings[FILTERED_ASPECT_RATIO]
+    print(len(public_domain_paintings))
+
+    public_domain_paintings.to_csv(
+        "../Database/metmuseum_filtered.csv", encoding='utf-8', index=False)
     print("filtered csv file ! ")
+
+
 def getImageURL(url):
     if not validators.url(url):
         print(f"url is not valid , {url}")
         return None
     try:
-        r = requests.get(url , headers={'User-Agent': 'Mozilla/5.0'})
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         html_page = html.unescape(r.text)
-        soup = BeautifulSoup(html_page,features="lxml")
+        soup = BeautifulSoup(html_page, features="lxml")
     except Exception as err:
         print("ERR OCCURED : \n ")
         print(err)
         return None
     URLS = []
-    for a in soup.find_all('a',{'class':'gtm__download__image'}, href=True):
+    for a in soup.find_all('a', {'class': 'gtm__download__image'}, href=True):
         URLS.append(a['href'])
-    if(len(URLS) == 0):
+    if (len(URLS) == 0):
         print(f"Could not get image URL from : {url}")
         return None
     return URLS[0]
@@ -58,17 +72,20 @@ def main():
     csv_file = Path("../Database/metmuseum_filtered.csv")
     if csv_file.is_file():
         paintings = pd.read_csv("../Database/metmuseum_filtered.csv")
+        paintings = paintings.reset_index(drop=True)
         for i in tqdm(range(len(paintings))):
             url = paintings.iloc[i]["Link Resource"]
-            print(paintings.iloc[i]["Link Resource"])
             image_url = getImageURL(url)
-            if(image_url is not None):
-                paintings.insert(i,"image_url",image_url)
+            print(image_url)
+            if (image_url is not None):
+                paintings.at[i, "image_url"] = image_url
+            if (i % 10 == 0 and i > 0):
+                paintings.to_csv(
+                    "../Database/metmuseum_filtered.csv", encoding='utf-8', index=False)
         paintings.to_csv("../Database/metmuseum_filtered.csv",encoding='utf-8', index=False)
     else:
         print("filtering csv file")
         filter()
-
 
 
 if __name__ == "__main__":
