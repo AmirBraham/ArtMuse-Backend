4529bb1f423b91120cfb26cfce2dd6f154b0f2ef
amirbraham <amirbrahamm@gmail.com>
2023-07-26 13:22:12 +0100

    setting wallpaper by url is buggy , need to implement preview before

--- artmuseapp/src/app/state/api.tsx
+++ artmuseapp/src/app/state/api.tsx
@@ -1,6 +1,8 @@
 import { API_BASE_URL } from "./init"; 
 import { Wallpaper } from "../types";
 export const getWallpaper = async (limit:number,page:number) => {
+  // majors changes needed here : 
+  // fetch images offline , if they don't exist use API to download 10 images
     if(page<1)
         page =1
     const response = await fetch(API_BASE_URL+ `paintings/?limit=${limit}&page=${page}`, {
