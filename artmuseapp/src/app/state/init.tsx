bdaa5e8f55cbdec321fb081e2ba9067d0e1d1bb7
amirbraham <amirbrahamm@gmail.com>
2023-07-29 00:00:54 +0100

    changed url

--- artmuseapp/src/app/state/init.tsx
+++ artmuseapp/src/app/state/init.tsx
@@ -1,4 +1,4 @@
 export const DEFAULT_INTERVAL_VALUE = 15
 
 const DEV = true
-export const API_BASE_URL = DEV ? "http://localhost:8000/api/" : "http://artmuse-2b28bb190030.herokuapp.com/api/"
+export const API_BASE_URL = DEV ? "http://localhost:8000/api/" : "https://artmuse-617f4c1e3849.herokuapp.com/api/"
