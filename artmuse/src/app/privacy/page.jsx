ac3c4438760a03275ba498ce57d98374943b7d37
amirbraham <amirbrahamm@gmail.com>
2023-08-04 11:27:58 +0100

    privacy policy for yu

---
+++ artmuse/src/app/privacy/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function Privacy() {
+   
+    return (
+       <p>
+        We do not collect any personal data or any data at all that could identify you personally.All we ask for is a good internet collection to download these wallpapers from our database into your laptop
+       </p>
+    )
+}
